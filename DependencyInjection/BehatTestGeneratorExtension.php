e929e680c6a8bb097531dd2110895bb06c053ab3
Kevin Bunel <kevinbunel@gmail.com>
2018-11-13 20:24:23 +0100

    update load extension

--- DependencyInjection/BehatTestGeneratorExtension.php
+++ DependencyInjection/BehatTestGeneratorExtension.php
@@ -9,7 +9,7 @@ class BehatTestGeneratorExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/Resources/config'));
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 		$loader->load('services.yaml');
 
 		$configuration = new Configuration();
